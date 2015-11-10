51d27bdd57139cfed78e3db2a160bd591598ce66
Scott Ivinza <Scott@MacBook-Pro-de-Scott.local>
2015-11-10 15:39:47 +0100

    Ajout de  super_agent

---
+++ super_agent.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+#!/usr/bin/env python3
+
+'''
+
+NAMES OF THE AUTHOR(S):
+Scott IVINZA MBE
+Jos ZIGABE
+GROUPE 29
+
+'''
+
+import avalam
+import minimax
+
+class SuperAgent(avalam.Agent, minimax.Game):
+
+    pass
